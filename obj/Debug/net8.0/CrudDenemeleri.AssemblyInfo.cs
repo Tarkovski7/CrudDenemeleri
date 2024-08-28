9411fc7a480773dc3a528067355fdb37dfcdcb2d
Tarkovski7 <trkbgr1618@gmail.com>
2024-08-29 02:38:16 +0300

    updated

--- obj/Debug/net8.0/CrudDenemeleri.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudDenemeleri.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudDenemeleri")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e372e66f1f0c104b50b5e95909c53a474f4adce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77aa564d3972df767422f01c22068b7965474324")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudDenemeleri")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudDenemeleri")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
